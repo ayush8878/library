9460a95435521d078f8a8e34c79c833be3cf387e
Ayush Sharma <ayush8878@gmail.com>
2021-05-02 12:23:26 +0530

    Create MaximumSumOfSubArrayOfSizeK.java

---
+++ Google Interview/MaximumSumOfSubArrayOfSizeK.java
@@ -0,0 +1,45 @@
+public class MaximumSumSubArrayOfSizeK
+{
+/*
+*This method will find maximum sum of subarray of size k using sliding window protocol
+*/
+ public static int findMaximumSumSubArrayOfSizeK(int []arr,int k)
+                 {
+                 int windowStart =0;
+                 int maxSum = 0;
+                 for( int windowEnd = 0;windowEnd<arr.length;windowEnd++)
+                      {
+                        if(windowEnd>=k-1)
+                           {  
+                            // find sum of window
+                            int windowSum = findWindowSum(arr,windowStart,windowEnd);
+                            if(windowSum > maxSum )
+                              {
+                              maxSum = windowSum ;
+                              }
+                            windowStart++;
+                            }
+                      }
+                      return maxSum;
+                 }
+/*
+*  This method will find window sum
+*/
+private static int findWindowSum(int []arr,int windowStart,int windowEnd)
+                 {
+                  int windowSum = 0;
+                  for(;windowStart<=windowEnd;windowStart++)
+                       {
+                   windowSum += arr[windowStart];
+                        }
+                  return windowSum;
+                  }
+
+public static void main(String args[])
+{
+int []arr = new int[]{1,3,2,6,-1,4,1,8,2};
+int k =5;
+int maxSum = findMaximumSumSubArrayOfSizeK(arr,k);
+System.out.print("Maximum sum of subarray of size:" +k+" is "+maxSum);
+}
+}
